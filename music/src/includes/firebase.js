92017a46e36ba4111419c4944e22fdd92397531b
sone <sthaya@gmail.com>
2022-12-31 19:53:48 -0800

    music, setup firebase for user registeration

---
+++ music/src/includes/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBW_EVJmR0Agfo9PkH6Sz7TkAkuKL-O1Ps",
+  authDomain: "music-vue-93015.firebaseapp.com",
+  projectId: "music-vue-93015",
+  storageBucket: "music-vue-93015.appspot.com",
+  appId: "1:104436155181:web:4548a1fc9aa231d6e3c9a4",
+};
+
+export default firebase.initializeApp(firebaseConfig);
