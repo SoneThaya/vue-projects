9375776777ef943cce722ae473c4cadaa95b45b7
sone <sthaya@gmail.com>
2023-01-12 21:18:59 -0800

    created navbar and movielist and moviecard components

--- movie-app/src/App.vue
+++ movie-app/src/App.vue
@@ -1,9 +1,13 @@
 <template>
+  <Navbar />
   <AsyncBanner :banner="bannerMovie" />
+  <MovieList :movies="movies" />
 </template>
 
 <script setup>
 import { ref, onMounted, defineAsyncComponent } from "vue";
+import Navbar from "./components/Navbar.vue";
+import MovieList from "./components/MovieList.vue";
 
 const AsyncBanner = defineAsyncComponent(() => {
   return import("./components/Banner.vue");
@@ -15,13 +19,13 @@ const bannerMovie = ref(null);
 const getMovies = async () => {
   movies.value = await fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${
-      import.meta.env.VITE_MOVIE_API_KEY
+      import.meta.env.VUE_MOVIE_API_KEY
     }`
   )
     .then((res) => res.json())
     .then((res) => res.results);
 };
-
+console.log(movies.value);
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
---
+++ movie-app/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav
+    class="flex text-white items-center gap-20 absolute left-0 right-0 mx-6 py-4 top-4 bg-transparent border-b-2 border-neutral-400"
+  >
+    <div class="flex text-3xl items-center font-bold gap-2">
+      <Icon icon="mingcute:movie-line" width="40" />
+      <span>MoVue</span>
+    </div>
+    <ul class="flex items-center gap-4 font-semibold">
+      <li>MoVues</li>
+      <li>Favorite MoVues</li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
