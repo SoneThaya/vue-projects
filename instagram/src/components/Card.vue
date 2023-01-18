a0630d76bdfecd154f33d4be13ddb0e13580c61b
sone <sthaya@gmail.com>
2023-01-17 20:59:58 -0800

    created card and timeline components

---
+++ instagram/src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <ACard hoverable style="width: 240px" class="card">
+    <template #cover>
+      <img alt="example" :src="post.url" />
+    </template>
+    <ACardMeta :title="post.username">
+      <template #description>{{ post.caption }}</template>
+    </ACardMeta>
+  </ACard>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps(["post"]);
+</script>
+
+<style scoped>
+.card {
+  margin-bottom: 20px;
+}
+</style>
---
+++ instagram/src/components/Timeline.vue
@@ -0,0 +1,36 @@
+<template>
+  <Container>
+    <div class="timeline-container">
+      <Card v-for="post in data" :key="post.id" :post="post" />
+    </div>
+  </Container>
+</template>
+
+<script setup>
+import Card from "./Card.vue";
+import Container from "./Container.vue";
+
+const data = [
+  {
+    id: 1,
+    username: "selena",
+    url: "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    caption: "awesome guys",
+  },
+  {
+    id: 2,
+    username: "weekend",
+    url: "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    caption: "hello guys",
+  },
+];
+</script>
+
+<style scoped>
+.timeline-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 0;
+}
+</style>
